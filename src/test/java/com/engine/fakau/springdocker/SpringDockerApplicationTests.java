12d2458c77d2361964f73a69ea30f0fd93092491
Laurent Kafka M <laurentkafka123@gmail.com>
2020-08-01 09:24:59 -0400

    init

---
+++ src/test/java/com/engine/fakau/springdocker/SpringDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engine.fakau.springdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
